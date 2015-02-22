1b7681cb895fd4b5613045dcca6914ad087c274d
Kunuk Nykjær <kunukn@gmail.com>
2015-02-22 11:47:44 +0100

    upd code style

--- src/content/text/app-data.js
+++ src/content/text/app-data.js
@@ -12,10 +12,10 @@ var Site = Site || {};
 Open the index.html with your browser.
 
   * Your browser is the server
-  * Your database and media library is the content folder
-  * Your Cms editor is your favorite text-editor.
+  * Your CMS is the content folder
+  * Your CMS editor is your favorite text-editor.
 
-To edit the content open `content/text/app-data.js` file and refresh your browser to see the update.
+To edit the content open the `content/text/app-data.js` file and refresh your browser to see the update.
             !*/
         }.parseMarkdown(),
 
